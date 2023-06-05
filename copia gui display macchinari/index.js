9f0e8b673a9ab2d4b59c0532c1b851cfeab119aa
cristianocatemario <cristiano.catemario@mpolo.it>
2023-06-05 16:43:14 +0200

    aggiunta tabellaServicesN

--- copia gui display macchinari/index.js
+++ copia gui display macchinari/index.js
@@ -1,12 +1,14 @@
 var settings = false;
 var services = false;
-var page = 0;
 var pageIndex = 0;
+
+var i = 0;
+var pageName = "tabellaService";
 
 function hideAll(){
   document.getElementById('tabellaPrincipale').setAttribute("hidden", "hidden");
   document.getElementById('tabellaImpostazioni').setAttribute("hidden", "hidden");
-  document.getElementById('tabellaServices').setAttribute("hidden", "hidden");
+  document.getElementById('tabellaServices0').setAttribute("hidden", "hidden");
 }
 
 function changeTitle(nomeProdotto) {
@@ -15,25 +17,30 @@ function changeTitle(nomeProdotto) {
 
 function showPage(tag){
   hideAll();
-  document.getElementById(tableimg.get(tag.id)).removeAttribute("hidden");
-  if(tag.id == "imgBS"){
+  document.getElementById(tableimg.get(tag.id)).removeAttribute("hidden"); //rendo visibile la pagina 
+  pageIndex = indextable.get(tableimg.get(tag.id));
+  
+  if(pageIndex == 0){
+    tag.src = "Resources/impostazioni_NB.png";
+  }  
+  else if(pageIndex == 1){
     tag.src = "Resources/indietro.png";
   }
+  else if(pageIndex == 2){
 
-
-
+  }
 }
 
 const tableimg = new Map([
   ["imgBS", "tabellaImpostazioni"], //associo all'immagine delle impostazioni, la pagina delle impostazioni
-  ["imgSRVC", "tabellaServices"],
-  ["lblSRVC", "tabellaServices"],
+  ["imgSRVC", "tabellaServices0"],
+  ["lblSRVC", "tabellaServices0"],
 ])
 
 const indextable = new Map([
-  [1, "tabellaImpostazioni"]
-  [2, "tabellaService"]
-  [3, "tabellaPrincipale"]
+  ["tabellaPrincipale", 0],
+  ["tabellaImpostazioni", 1],
+  ["tabellaServices0", 2],
 ])
 
 
