8bb723e4b4535e17be85da8bb4bd1329a5fe977b
cristianocatemario <cristiano.catemario@mpolo.it>
2023-06-06 12:20:14 +0200

    Aggiunta cambio pagina di tabellaServices
    Da rivedere per il corretto funzionamento delle frecce (in particolare per nascondere le altre tabelleServices)

--- copia gui display macchinari/index.js
+++ copia gui display macchinari/index.js
@@ -3,12 +3,17 @@ var services = false;
 var pageIndex = 0;
 
 var i = 0;
-var pageName = "tabellaService";
+var charAt = 0;
+
+var nextPage = 1; 
+var previousPage;
 
 function hideAll(){
   document.getElementById('tabellaPrincipale').setAttribute("hidden", "hidden");
   document.getElementById('tabellaImpostazioni').setAttribute("hidden", "hidden");
-  document.getElementById('tabellaServices0').setAttribute("hidden", "hidden");
+  document.getElementById('tabellaServices1').setAttribute("hidden", "hidden");
+  document.getElementById('prev').setAttribute("hidden", "hidden");
+  document.getElementById('next').setAttribute("hidden", "hidden");
 }
 
 function changeTitle(nomeProdotto) {
@@ -27,20 +32,63 @@ function showPage(tag){
     tag.src = "Resources/indietro.png";
   }
   else if(pageIndex == 2){
-
+    document.getElementById('prev').removeAttribute("hidden")
+    document.getElementById('next').removeAttribute("hidden")
   }
+}
+
+function showPageByNumber(ts, component){
+  var pagina;
+
+  if(component.id == "next"){ //pag succ
+    nextPage++;
+    pagina = ts + nextPage; //concat della stringa tabellaServices e 1 = tabellaServices1
+  }
+
+  if(component.id == "prev"){ //pag precedente
+    if(nextPage == 0 ){
+      document.getElementById("tabellaServices1").removeAttribute("hidden");
+    }
+    else{
+      nextPage--;
+      pagina = ts + nextPage;
+      previousPage = ts + nextPage--;
+      hideAll();
+    }
+  }
+
+
+  // document.getElementById(previousPage).setAttribute("hidden", "hidden");
+  hideAll();
+  document.getElementById('prev').removeAttribute("hidden");
+  document.getElementById('next').removeAttribute("hidden");
+  document.getElementById(pagina).removeAttribute("hidden");
 }
 
 const tableimg = new Map([
   ["imgBS", "tabellaImpostazioni"], //associo all'immagine delle impostazioni, la pagina delle impostazioni
-  ["imgSRVC", "tabellaServices0"],
-  ["lblSRVC", "tabellaServices0"],
+  ["imgSRVC", "tabellaServices1"],
+  ["lblSRVC", "tabellaServices1"],
 ])
 
 const indextable = new Map([
-  ["tabellaPrincipale", 0],
+  ["tabellaPrincipale", 1],
   ["tabellaImpostazioni", 1],
-  ["tabellaServices0", 2],
+  ["tabellaServices1", 2],
+  
+])
+
+const tabellaServicesN = new Map([
+  ["tabellaServices2", 2],
+  ["tabellaServices3", 3],
+  ["tabellaServices4", 4],
+  ["tabellaServices5", 5],
+  ["tabellaServices6", 6],
+  ["tabellaServices7", 7],
+  ["tabellaServices8", 8],
+  ["tabellaServices9", 9],
+  ["tabellaServices10", 10],
+  ["tabellaServices11", 11],
 ])
 
 
